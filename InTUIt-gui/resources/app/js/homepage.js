9615fd20a6b255923a9ac20b7c7570db1de6d1f2
Christopher Franklyn <Christopher Franklyn>
2016-11-02 18:09:29 -0500

    Modals and submit button are connected to fully make an NDF

--- InTUIt-gui/resources/app/js/homepage.js
+++ InTUIt-gui/resources/app/js/homepage.js
@@ -14,11 +14,13 @@ var qs = require('querystring');
 
 var username = 'generic'; //Variable for logged in user. Default is 'generic'
 var networkName = 'network'; //Variable for current network. Default is 'network'
+var ndfFilename = username + networkName + '.ndf';
 
 var areaList = new Array(); //Array of all areas in the Network
 var deviceList = new Array(); //Array of all ACUs in the Network
 
 var deviceTable;
+var changesTable;
 
 
 //Network Class----------------------------------------------------------------
@@ -48,30 +50,29 @@ var Network = function (networkName) {
 }
 //-----------------------------------------------------------------------------
 
+
 //Area Class-------------------------------------------------------------------
-var Area = function (areaName) {
+var Area = function(areaName) {
     this.areaName = areaName;
-
     this.acuList = new Array(); //Array of all ACUs in the Area
-
 
     this.addACU = function addACU(acu) {
         this.acuList.push(acu);
     }
 
     this.printArea = function printArea() {
-        var areaString = "\"" + this.areaName + "\" :{";
-        for(var i = 0; index < acuList.length; i++){
-            areaString += acuList[i].printACU();
+        var areaString = "\"" + this.areaName + "\": {";
+        for(var i = 0; i < this.acuList.length; i++){
+            areaString += this.acuList[i].printACU();
         }
-        areaString += "}"
+        areaString += "},"
         return areaString;
     }
 
     this.printAreaPolicies = function printAreaPolicies() {
-        var areaString = "\"" + this.areaName + "\" :{";
-        for(var i = 0; index < acuList.length; i++){
-            areaString += acuList[i].printACUPolicies();
+        var areaString = "\"" + this.areaName + "\": {";
+        for(var i = 0; i < this.acuList.length; i++){
+            areaString += this.acuList[i].printACUPolicies();
         }
         areaString += "}"
         return areaString;
@@ -79,14 +80,14 @@ var Area = function (areaName) {
 }
 //-----------------------------------------------------------------------------
 
+
 //ACU Class--------------------------------------------------------------------
-var ACU = function (name, states, dependencies, actions) {
-    this.name = name;
+var ACU = function (acuName, states, dependencies, actions, area) {
+    this.acuName = acuName;
     this.states = states;
     this.dependencies = dependencies;
     this.actions = actions;
-    this.area = 'areaName'; //temporarily hard coded in
-
+    this.area = area;
     this.policyList = new Array(); //Array of all Policies associated to ACU
 
     this.addPolicy = function addPolicy(Policy) {
@@ -94,20 +95,21 @@ var ACU = function (name, states, dependencies, actions) {
     }
 
     this.printACU = function printACU(){
-        return "\"" + this.name + "\":{\"states\": [" + this.states + "], \"actions\": [" + this.actions +
-		"], \"dependencies\": [" + this.dependencies + "]}";
+        return "\"" + this.acuName + "\": {\"Dependencies\": [" + this.dependencies + "], \"States\": [" + this.states + "], \"Actions\": [" + this.actions +
+		"]},";
     }
 
     this.printACUPolicies = function printACUPolicies() {
-        var ACUString = "\"" + this.name + "\" :[";
-        for(var i = 0; index < policyList.length; i++){
-            ACUString += policyList[i].printPolicy();   //*****Coma Syntax Error for NDF*****
+        var ACUString = "\"" + this.acuName + "\" :[";
+        for(var i = 0; i < this.policyList.length; i++){
+            ACUString += this.policyList[i].printPolicy();   //*****Coma Syntax Error for NDF*****
         }
         ACUString += "]"
-        return areaString;
+        return ACUString;
     }
 }
 //-----------------------------------------------------------------------------
+
 
 //Policy Class-----------------------------------------------------------------
 var Policy = function (area, device, givenStates, command) {
@@ -129,59 +131,86 @@ var Policy = function (area, device, givenStates, command) {
 //-----------------------------------------------------------------------------
 
 
-//script that executes once index page is fully loaded
+//script that executes once homepage is fully loaded
 $(document).ready(function() {
   var queryString = window.location.search
   username = getQueryVariable('userName', queryString);
   networkName = getQueryVariable('networkName', queryString);
-  var ndfFilename = username + '-' + networkName + '.ndf'; //file name to write NDF to
+  ndfFilename = username + '-' + networkName + '.ndf'; //file name to write NDF to
 
   //Populate the Username and Network Fields bassed on Login
   $('#user-name').html('User: ' + username);
   $('#network-name').html('Network: ' + networkName);
 
-  $('#changes').load('./html/current_changes.html');
-  $('#add-area-load').load('./html/modals/add_area.html');
-  $('#add-device-load').load('./html/modals/add_device.html');
-  $('#add-policy-load').load('./html/modals/create_policy.html');
+  //Creates the changes datatable
+  changesTable = $('#changesTable').DataTable({
+    "paging": true,
+    "iDisplayLength": 10,
+    "lengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "All"]],
+    "responsive": true,
+    "autoWidth": false,
+    "language": {"emptyTable": "No changes have been made"},
+    "order": [[0, 'desc']],
+    "columns": [
+        { "width": "33%" },
+        { "width": "33%" },
+        { "width": "33%" }
+    ],
+    "ajax": {
+      "url": './json/changes.json',
+      "dataSrc": 'changeData'
+    }
+  });
 
-  $('#addDeviceFor').submit(function(e){
-    console.log('test');
+  //event fires upon adding a device
+  $('#addDeviceForm').submit(function(e){
     e.preventDefault(); //prevent form from redirect
     setTimeout(function(){ //allow addDevice to execute before refresh
       deviceTable.ajax.reload();
-      $('#addDeviceForm')[0].reset();
+      $('#addDeviceForm')[0].reset(); //reset form fields
     }, 100);
     addDevice();
+	$('#deviceName').focus();
   });
 
+  //event fires upon adding an area
   $('#addAreaForm').submit(function(e){
-    console.log("test");
     e.preventDefault(); //prevent form from redirect
+	addArea();
     this.reset(); //resets the fields within the form
+	$('#areaName').focus();
+  });
+
+  //event fires upon adding a policy
+  $('#createPolicyForm').submit(function(e){
+	e.preventDefault();
+	addPolicy();
+	this.reset();
+	$('#policyArea').focus();
   });
 });
 
-//Function to construct the NDF file for a user network
-$('#submitNDF').click(function buildNDF() {
-  console.log("button works");
-  var stream = fs.createWriteStream('./resources/app/ndf/' + ndfFilename);
-  stream.write(username + ', ' + networkName + '\n');
-  for (var i; i < areaList.length; i++) {
-    stream.write(i.printArea());
-  }
-  stream.end();
-});
 
-//ADDING A DEVICE INTO NETWORK
+//Adding an area into the network
+function addArea() {
+  areaList.push(new Area($('#areaName').val()));
+  $('#areaSelect').empty();
+  for (var i = 0; i < areaList.length; i++) {
+	  var area = areaList[i];
+	  $('#areaSelect').append('<option value="' + area.areaName + '">' + area.areaName +'</option>');
+  }
+}
+
+//Adding a device into the network
 function addDevice() {
-  var tempDevice = new ACU($('#deviceName').val(), $('#deviceStates').val(), $('#deviceDependencies').val(), $('#deviceActions').val());
-  deviceList.push(tempDevice); //Adds device to JS array of device objects
+  var tempDevice = new ACU($('#deviceName').val(), $('#deviceStates').val(), $('#deviceDependencies').val(), $('#deviceActions').val(), $('#areaSelect').val());
+  var deviceArea = findArea($('#areaSelect').val());
+  deviceArea.addACU(tempDevice);
+
   var date = new Date();
 
   //function call to add the device to the stored json of devices
   $.getJSON("./json/devices.json", function(json) {
-    console.log(json); // this will show the info it in firebug console
     var deviceJSON = [date.toLocaleString(), $('#deviceName').val(), $('#deviceStates').val(), $('#deviceActions').val(), $('#deviceDependencies').val()];
     json.tableData.push(deviceJSON);
     var stream = fs.createWriteStream('./resources/app/json/devices.json');
@@ -190,14 +219,41 @@ function addDevice() {
   });
 }
 
-//ADDING AN AREA INTO NETWORK
-function addArea() {
-  var tempArea = new Area($('#areaName').val());
-  areaList.push(tempArea); //Adds area to JS array of area objects
+//Adding a policy to an existing ACU
+function addPolicy() {
+  var tempPolicy = new Policy($('#policyArea').val(), $('#policyDevice').val(), $('#givenStates').val(), $('#associatedCommand').val());
+  var policyArea = findArea($('#policyArea').val());
+  var policyACU = findACU($('#policyDevice').val(), policyArea);
+  policyACU.addPolicy(tempPolicy);
 }
+
+//Function to construct the NDF file for a user network
+$('#submitNDF').click(function buildNDF() {
+  var stream = fs.createWriteStream('./resources/app/' + ndfFilename);
+  stream.write(username + ',' + networkName + '\n{');
+  for (var i = 0; i < areaList.length; i++) {
+    stream.write(areaList[i].printArea());
+  }
+  stream.write('}\n{');
+  for (var i = 0; i < areaList.length; i++) {
+	  stream.write(areaList[i].printAreaPolicies());
+  }
+  stream.write('}')
+  stream.end();
+});
 
 //Create Device Datatable when button to summon modal is clicked
 $('#add-device-button').click(function() {
+  if (areaList.length == 0) {
+    //e.preventDefault();
+    alert("No areas have been created. Please create one");
+  }
+  else {
+    $('#add-device-modal').modal({
+      focus: true
+    });
+  }
+
   if (!($.fn.dataTable.isDataTable('#deviceTable'))){ //if the datatable isn't created, make it
     deviceTable = $('#deviceTable').DataTable({
   	  "paging": true,
@@ -205,6 +261,7 @@ $('#add-device-button').click(function() {
       "lengthMenu": [[5, 10, 25, 50, 100, -1], [5, 10, 25, 50, 100, "All"]],
       "responsive": true,
       "autoWidth": false,
+      "language": {"emptyTable": "No ACUs have been created"},
       "order": [[0, 'desc']],
       "columns": [
           { "width": "12%" },
@@ -220,6 +277,39 @@ $('#add-device-button').click(function() {
     });
   }
 });
+
+$('#add-policy-button').click(function() {
+  if (areaList.length == 0) {
+    //e.preventDefault();
+    alert("No areas have been created. Please create one");
+  }
+  else {
+    $('#create-policy-modal').modal({
+      focus: true
+    });
+  }
+});
+
+//Finds a created area in the list of areas
+function findArea(name) {
+	for (var i = 0; i < areaList.length; i++) {
+		if (areaList[i].areaName == name){
+			return areaList[i];
+		}
+	}
+	console.log("findArea: Could not find area specified")
+}
+
+//Finds an ACU in a given Area
+function findACU(name, Area) {
+	var acuList = Area.acuList;
+	for (var i = 0; i < acuList.length; i++) {
+		if (acuList[i].acuName == name){
+			return acuList[i];
+		}
+	}
+	console.log("findACU: Could not find ACU specified")
+}
 
 //Returns query string value given
 function getQueryVariable(variable, queryString) {
